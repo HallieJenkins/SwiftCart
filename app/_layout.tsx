d39638e545837afdb152a529f7389ba246a95e9a
HallieJenkins <halliej@bellsouth.net>
2025-04-21 23:02:12 -0400

    Diff UI Setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,9 @@
 import '../global.css';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
-
-import { useColorScheme, useInitialAndroidBarSync } from '@/lib/useColorScheme';
-import { NAV_THEME } from '@/theme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -12,18 +11,9 @@ export {
 } from 'expo-router';
 
 export default function RootLayout() {
-  useInitialAndroidBarSync();
-  const { colorScheme, isDarkColorScheme } = useColorScheme();
 
   return (
     <>
-      <StatusBar
-        key={`root-status-bar-${isDarkColorScheme ? 'light' : 'dark'}`}
-        style={isDarkColorScheme ? 'light' : 'dark'}
-      />
-
-      <NavThemeProvider value={NAV_THEME[colorScheme]} children={undefined}>
-      </NavThemeProvider>
     </>
   );
 }
